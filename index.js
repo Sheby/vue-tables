9d17e8aaa99bbc15466ace9de1d3fae58f9d47f4
Antun Šebalj <sebaljantun@gmail.com>
2016-11-22 15:16:34 +0100

    all done for now

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 String.prototype.ucfirst = require('./lib/helpers/ucfirst');
 Array.prototype.diff = require('./lib/helpers/array-diff');
 
+//register partial directive globally
+Vue.directive('partial', require('./lib/directives/partial'));
+
 exports.client = require('./lib/v-client-table');
 exports.server = require('./lib/v-server-table');
